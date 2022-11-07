13ace578549386511ac2be98aabe7cb5e0c4acc6
Micael Eid <Mikaeleid@hotmail.com>
2022-11-07 14:19:43 +0000

    Criação Projeto

---
+++ cmake-build-debug/CMakeFiles/POO_TP_22_23.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POO_TP_22_23.dir/main.cpp.obj"
+  "POO_TP_22_23.exe"
+  "POO_TP_22_23.exe.manifest"
+  "POO_TP_22_23.pdb"
+  "libPOO_TP_22_23.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POO_TP_22_23.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
