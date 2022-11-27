842eff54822bab5c53cf7d13b5c9a5d7d503f3dd
Micael Eid <Mikaeleid@hotmail.com>
2022-11-27 01:46:49 +0000

    att MIKE

--- cmake-build-debug/CMakeFiles/POO_TP_22_23.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/POO_TP_22_23.dir/DependInfo.cmake
@@ -8,11 +8,11 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/Gonsalo/Documents/GitHub/POO-TP-22-23/Alimentos.cpp" "CMakeFiles/POO_TP_22_23.dir/Alimentos.cpp.obj" "gcc" "CMakeFiles/POO_TP_22_23.dir/Alimentos.cpp.obj.d"
-  "C:/Users/Gonsalo/Documents/GitHub/POO-TP-22-23/Animais.cpp" "CMakeFiles/POO_TP_22_23.dir/Animais.cpp.obj" "gcc" "CMakeFiles/POO_TP_22_23.dir/Animais.cpp.obj.d"
-  "C:/Users/Gonsalo/Documents/GitHub/POO-TP-22-23/Reserva.cpp" "CMakeFiles/POO_TP_22_23.dir/Reserva.cpp.obj" "gcc" "CMakeFiles/POO_TP_22_23.dir/Reserva.cpp.obj.d"
-  "C:/Users/Gonsalo/Documents/GitHub/POO-TP-22-23/Simulador.cpp" "CMakeFiles/POO_TP_22_23.dir/Simulador.cpp.obj" "gcc" "CMakeFiles/POO_TP_22_23.dir/Simulador.cpp.obj.d"
-  "C:/Users/Gonsalo/Documents/GitHub/POO-TP-22-23/main.cpp" "CMakeFiles/POO_TP_22_23.dir/main.cpp.obj" "gcc" "CMakeFiles/POO_TP_22_23.dir/main.cpp.obj.d"
+  "C:/ISEC/1Semestre/POO/TP/POO-TP-22-23/Alimentos.cpp" "CMakeFiles/POO_TP_22_23.dir/Alimentos.cpp.obj" "gcc" "CMakeFiles/POO_TP_22_23.dir/Alimentos.cpp.obj.d"
+  "C:/ISEC/1Semestre/POO/TP/POO-TP-22-23/Animais.cpp" "CMakeFiles/POO_TP_22_23.dir/Animais.cpp.obj" "gcc" "CMakeFiles/POO_TP_22_23.dir/Animais.cpp.obj.d"
+  "C:/ISEC/1Semestre/POO/TP/POO-TP-22-23/Reserva.cpp" "CMakeFiles/POO_TP_22_23.dir/Reserva.cpp.obj" "gcc" "CMakeFiles/POO_TP_22_23.dir/Reserva.cpp.obj.d"
+  "C:/ISEC/1Semestre/POO/TP/POO-TP-22-23/Simulador.cpp" "CMakeFiles/POO_TP_22_23.dir/Simulador.cpp.obj" "gcc" "CMakeFiles/POO_TP_22_23.dir/Simulador.cpp.obj.d"
+  "C:/ISEC/1Semestre/POO/TP/POO-TP-22-23/main.cpp" "CMakeFiles/POO_TP_22_23.dir/main.cpp.obj" "gcc" "CMakeFiles/POO_TP_22_23.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links.
